c4f538193679d66546a2ab916b65205c1c94e983
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2023-03-27 00:10:12 +0200

    Add files via upload

---
+++ AplikacijaZaRestoran/Juha.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace AplikacijaZaRestoran
+{
+    partial class Juha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rajcicaJuha = new System.Windows.Forms.Button();
+            this.tikviceJuha = new System.Windows.Forms.Button();
+            this.gljiveJuha = new System.Windows.Forms.Button();
+            this.ribljaJuha = new System.Windows.Forms.Button();
+            this.povrtnaJuha = new System.Windows.Forms.Button();
+            this.govedaJuha = new System.Windows.Forms.Button();
+            this.pilecaJuha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rajcicaJuha
+            // 
+            this.rajcicaJuha.Location = new System.Drawing.Point(319, 146);
+            this.rajcicaJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rajcicaJuha.Name = "rajcicaJuha";
+            this.rajcicaJuha.Size = new System.Drawing.Size(124, 98);
+            this.rajcicaJuha.TabIndex = 22;
+            this.rajcicaJuha.Text = "Juha od rajčice";
+            this.rajcicaJuha.UseVisualStyleBackColor = true;
+            // 
+            // tikviceJuha
+            // 
+            this.tikviceJuha.Location = new System.Drawing.Point(167, 146);
+            this.tikviceJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tikviceJuha.Name = "tikviceJuha";
+            this.tikviceJuha.Size = new System.Drawing.Size(124, 98);
+            this.tikviceJuha.TabIndex = 21;
+            this.tikviceJuha.Text = "Krem juha od tikvica";
+            this.tikviceJuha.UseVisualStyleBackColor = true;
+            this.tikviceJuha.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // gljiveJuha
+            // 
+            this.gljiveJuha.Location = new System.Drawing.Point(15, 146);
+            this.gljiveJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gljiveJuha.Name = "gljiveJuha";
+            this.gljiveJuha.Size = new System.Drawing.Size(124, 98);
+            this.gljiveJuha.TabIndex = 20;
+            this.gljiveJuha.Text = "Krem juha od gljiva";
+            this.gljiveJuha.UseVisualStyleBackColor = true;
+            // 
+            // ribljaJuha
+            // 
+            this.ribljaJuha.Location = new System.Drawing.Point(468, 27);
+            this.ribljaJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ribljaJuha.Name = "ribljaJuha";
+            this.ribljaJuha.Size = new System.Drawing.Size(124, 98);
+            this.ribljaJuha.TabIndex = 19;
+            this.ribljaJuha.Text = "Riblja juha";
+            this.ribljaJuha.UseVisualStyleBackColor = true;
+            // 
+            // povrtnaJuha
+            // 
+            this.povrtnaJuha.Location = new System.Drawing.Point(319, 27);
+            this.povrtnaJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.povrtnaJuha.Name = "povrtnaJuha";
+            this.povrtnaJuha.Size = new System.Drawing.Size(124, 98);
+            this.povrtnaJuha.TabIndex = 18;
+            this.povrtnaJuha.Text = "Povrtna juha";
+            this.povrtnaJuha.UseVisualStyleBackColor = true;
+            // 
+            // govedaJuha
+            // 
+            this.govedaJuha.Location = new System.Drawing.Point(167, 27);
+            this.govedaJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.govedaJuha.Name = "govedaJuha";
+            this.govedaJuha.Size = new System.Drawing.Size(124, 98);
+            this.govedaJuha.TabIndex = 17;
+            this.govedaJuha.Text = "Goveđa juha";
+            this.govedaJuha.UseVisualStyleBackColor = true;
+            this.govedaJuha.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pilecaJuha
+            // 
+            this.pilecaJuha.Location = new System.Drawing.Point(15, 27);
+            this.pilecaJuha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pilecaJuha.Name = "pilecaJuha";
+            this.pilecaJuha.Size = new System.Drawing.Size(124, 98);
+            this.pilecaJuha.TabIndex = 16;
+            this.pilecaJuha.Text = "Pileća juha";
+            this.pilecaJuha.UseVisualStyleBackColor = true;
+            this.pilecaJuha.Click += new System.EventHandler(this.pilecaJuha_Click);
+            // 
+            // Juha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 518);
+            this.Controls.Add(this.rajcicaJuha);
+            this.Controls.Add(this.tikviceJuha);
+            this.Controls.Add(this.gljiveJuha);
+            this.Controls.Add(this.ribljaJuha);
+            this.Controls.Add(this.povrtnaJuha);
+            this.Controls.Add(this.govedaJuha);
+            this.Controls.Add(this.pilecaJuha);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Juha";
+            this.Text = "Juha";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button rajcicaJuha;
+        private System.Windows.Forms.Button tikviceJuha;
+        private System.Windows.Forms.Button gljiveJuha;
+        private System.Windows.Forms.Button ribljaJuha;
+        private System.Windows.Forms.Button povrtnaJuha;
+        private System.Windows.Forms.Button govedaJuha;
+        private System.Windows.Forms.Button pilecaJuha;
+    }
+}
---
+++ AplikacijaZaRestoran/Juha.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacijaZaRestoran
+{
+    public partial class Juha : Form
+    {
+        public Juha()
+        {
+            InitializeComponent();
+        }
+
+        private Pocetna myForm;
+
+        public Juha(Pocetna form1)
+        {
+            InitializeComponent();
+            myForm = form1;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pilecaJuha_Click(object sender, EventArgs e)
+        {
+            // Get the DataGridView object from the first form.
+            DataGridView dgv = myForm.Narudzba;
+
+            // Create a new row.
+            DataGridViewRow row = new DataGridViewRow();
+
+            // Add data to the row.
+            DataGridViewCell cell1 = new DataGridViewTextBoxCell();
+            cell1.Value = "Pileća juha";
+            row.Cells.Add(cell1);
+
+            DataGridViewCell cell2 = new DataGridViewTextBoxCell();
+            cell2.Value = "1";
+            row.Cells.Add(cell2);
+
+            DataGridViewCell cell3 = new DataGridViewTextBoxCell();
+            cell2.Value = "";
+            row.Cells.Add(cell3);
+
+            // Add the new row to the DataGridView.
+            dgv.Rows.Add(row);
+        }
+    }
+}
